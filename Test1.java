b8468838114c29e37cebbe8f7a08f4c5f1807b20
pujariakshay341 <pujariakshay341@gmail.com>
2026-07-23 12:22:44 +0530

    Test Code pushing

---
+++ Test1.java
@@ -0,0 +1,33 @@
+package testPackege;
+
+public class Test1 
+{
+	public static void main(String[] args) 
+	{
+		String name="HeXaEAre1";
+		
+		StringBuilder sb= new StringBuilder();
+		
+		for(int i=0;i<name.length();i++)
+		{
+			char ch=name.charAt(i);
+			
+			if(Character.isUpperCase(ch))
+			{
+				sb.append(Character.toLowerCase(ch));
+			}
+			else if(Character.isLowerCase(ch))
+			{
+				sb.append(Character.toUpperCase(ch));
+			}
+			else
+			{
+				sb.append(ch);
+			}
+			
+		}
+		System.out.println(name);
+		System.out.println(sb);
+	}
+
+}
